f36d16345aa2fb6d15feca36f0b8cd8f68aa6925
unknown <sabokyle12@gmail.com>
2021-06-16 19:36:45 -0500

    added data

--- grab_data.R
+++ grab_data.R
@@ -1,2 +1,12 @@
 library(tidyverse)
 library(rvest)
+
+raw_data <- read_html("https://en.wikipedia.org/wiki/UEFA_Euro_2020_squads")
+raw_data
+x <- raw_data %>%
+  html_elements("td:nth-child(4)") %>%
+  html_text2() %>%
+  as_tibble() 
+
+write_csv(x ,"birthdates.csv")
+
